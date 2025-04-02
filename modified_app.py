44e52669b18266bbddbac37ea18f0ed9c56fdc28
Kunal Kulkarni <105601900+coderkun12@users.noreply.github.com>
2025-04-02 15:21:15 +0530

    Update modified_app.py

--- modified_app.py
+++ modified_app.py
@@ -6,11 +6,13 @@ from routes.routes import routes_bp
 import os
 import secrets
 import random
+from services.database_service import db_cursor, db_connection 
 
 random.seed(42)
 app=Flask(__name__)
 app.secret_key=secrets.token_hex(32)
 app.register_blueprint(routes_bp)
+
 
 if __name__=="__main__":
     app.run(debug=True)
