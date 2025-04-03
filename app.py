e754678094232e7844e665eb64c6c71474c75848
Kunal Kulkarni <105601900+coderkun12@users.noreply.github.com>
2025-04-03 14:57:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import secrets
 import random
 from services.database_service import db_cursor, db_connection 
 
-random.seed(42)
-app=Flask(__name__)
-app.secret_key=secrets.token_hex(32)
-app.register_blueprint(routes_bp)
+random.seed(42) # Ensures that the secret keys generated stay consistent
+app=Flask(__name__) # Initialization of Flask App.
+app.secret_key=secrets.token_hex(32) # Generate the secret key for the Flask App.
+app.register_blueprint(routes_bp) # Integrates a modular set of routes and resources (a Blueprint) into a Flask application.
 
 
 if __name__=="__main__":
