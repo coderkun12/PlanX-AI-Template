9455784c83b97104b56e9433cb398486037e6d20
Kunal Kulkarni <105601900+coderkun12@users.noreply.github.com>
2025-04-03 14:44:29 +0530

    Update routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify, send_from_directory, session
 from controller.user_controller import signup_logic, login_logic, generate_message,check_auth_logic
 import os
-from services.database_service import db_cursor,db_connection
 import secrets
 import jwt
 
@@ -19,15 +18,13 @@ def serve():
 @routes_bp.route('/api/signup', methods=['POST'])
 def signup():
     data = request.get_json()
-    result, status_code = signup_logic(db_cursor, data) # pass the cursor
-    if status_code == 201:
-        db_connection.commit()
+    result, status_code = signup_logic(data) # pass the cursor
     return jsonify(result), status_code
 
 @routes_bp.route('/api/login', methods=['POST'])
 def login():
     data = request.get_json()
-    result, status_code = login_logic(db_cursor, data, JWT_SECRET) #pass the cursor
+    result, status_code = login_logic(data, JWT_SECRET) #pass the cursor
     if status_code == 200:
         session['jwt_token'] = result['token']
     return jsonify(result), status_code
@@ -35,7 +32,7 @@ def login():
 @routes_bp.route('/api/check-auth', methods=['GET'])
 def check_auth():
     token = session.get('jwt_token')
-    result, status_code = check_auth_logic(db_cursor, token, JWT_SECRET) #pass the cursor
+    result, status_code = check_auth_logic(token, JWT_SECRET) #pass the cursor
     if status_code != 200:
         session.pop('jwt_token', None)
     return jsonify(result), status_code
