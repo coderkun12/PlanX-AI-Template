751b8c32f7a9f2a57512013d61f8f8794f914351
Kunal Kulkarni <105601900+coderkun12@users.noreply.github.com>
2025-04-03 14:45:56 +0530

    Update database_service.py

--- services/database_service.py
+++ services/database_service.py
@@ -1,17 +1,15 @@
 # services/database_service.py
-import mysql.connector
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from config.connection import DATABASE_CONFIG
 
 try:
-    db_connection = mysql.connector.connect(
-        host=DATABASE_CONFIG["host"],
-        user=DATABASE_CONFIG["user"],
-        password=DATABASE_CONFIG["password"],
-        database=DATABASE_CONFIG["database"]
-    )
-    db_cursor = db_connection.cursor(dictionary=True)
-    print("MySQL connection successful")
+    engine = create_engine(f"mysql+mysqldb://{DATABASE_CONFIG['user']}:{DATABASE_CONFIG['password']}@{DATABASE_CONFIG['host']}/{DATABASE_CONFIG['database']}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    print("MySQL connection successful (SQLAlchemy)")
 except Exception as e:
-    print(f"MySQL connection error: {e}")
-    db_connection = None
-    db_cursor = None
+    print(f"MySQL connection error (SQLAlchemy): {e}")
+    engine = None
+    session = None
